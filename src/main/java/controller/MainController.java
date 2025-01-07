06111d5e468cf34f3801ef32b8217508b93ff4a5
srhrsb <sebastian.boettcher2@srh.de>
2025-01-07 14:52:52 +0100

    Attribute im Controller hinzugefügt

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,14 +1,56 @@
 package controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField treeIdTf;
+
+    @FXML
+    private TextField treeNameTf;
+
+    @FXML
+    private TextField treeAgeTf;
+
+    @FXML
+    private ComboBox treeFamilyCombo;
+
+    @FXML
+    private CheckBox treeSickCheckbox;
+
+    //Park Attribute
+    @FXML
+    private TextField parkIdTf;
+
+    @FXML
+    private TextField parkNameTf;
+
+    @FXML
+    private TableView parkTableView;
+
+
+    /**
+     * Wird aufgerufen wenn der Button "Baum hinzufügen" gedrückt wird
+     */
+    @FXML
+    protected void onClickTreeAdd(){
+
     }
+
+    /**
+     * Wird aufgerufen wenn der Button "Bäume des Parks auflisten" gedrückt wird
+     */
+    @FXML
+    protected void onClickListPark(){
+
+    }
+
+
+
+
+
+
+
 }
